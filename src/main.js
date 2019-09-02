235d2734af1881d255bf5d5b5f2bcb6f5045a534
崔苗苗 <1940803799@qq.com>
2019-09-02 16:45:34 +0800

    登录模块-页面布局及样式

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './style/index.less'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>login</div>
+  <div class="login">
+      <el-card class="card">
+          <div class="logo">
+              <img src="../../assets/img/logo_index.png" alt="">
+          </div>
+          <el-form>
+              <el-form-item>
+                  <el-input placeholder="请输入手机号"></el-input>
+              </el-form-item>
+              <el-form-item>
+                  <el-input placeholder="请输入验证码" class="inp2"></el-input>
+                  <el-button class="btn1">发送验证码</el-button>
+              </el-form-item>
+              <el-form-item>
+                  <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+              </el-form-item>
+              <el-form-item>
+                  <el-button type="primary" class="btn2">登录</el-button>
+              </el-form-item>
+          </el-form>
+      </el-card>
+      </div>
 </template>
 
 <script>
@@ -8,6 +29,35 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.login{
+    height: 100vh;
+    background-image: url('../../assets/img/login_bg.jpg');
+    background-size: cover;
+    position: relative;
+    .card{
+        width: 450px;
+        height: 350px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
+        .logo{
+            text-align: center;
+            margin-bottom: 15px;
+            img{
+                height: 45px;
+            }
+        }
+        .inp2{
+            width: 70%;
+        }
+        .btn1{
+            float: right;
+        }
+        .btn2{
+            width: 100%;
+        }
+    }
+}
 </style>

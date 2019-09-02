8e4312974d22f04c00d8edf06b00ee5999ec4ade
崔苗苗 <1940803799@qq.com>
2019-09-02 17:25:06 +0800

    登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,19 +4,19 @@
           <div class="logo">
               <img src="../../assets/img/logo_index.png" alt="">
           </div>
-          <el-form>
-              <el-form-item>
-                  <el-input placeholder="请输入手机号"></el-input>
+          <el-form :model="loginform" :rules="loginrules" ref="login">
+              <el-form-item prop="mobile">
+                  <el-input placeholder="请输入手机号" v-model="loginform.mobile"></el-input>
               </el-form-item>
-              <el-form-item>
-                  <el-input placeholder="请输入验证码" class="inp2"></el-input>
+              <el-form-item prop="number">
+                  <el-input placeholder="请输入验证码" class="inp2" v-model="loginform.number"></el-input>
                   <el-button class="btn1">发送验证码</el-button>
               </el-form-item>
-              <el-form-item>
-                  <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+              <el-form-item prop="status">
+                  <el-checkbox v-model="loginform.status">我已阅读并同意用户协议和隐私条款</el-checkbox>
               </el-form-item>
               <el-form-item>
-                  <el-button type="primary" class="btn2">登录</el-button>
+                  <el-button type="primary" class="btn2" @click="checkall">登录</el-button>
               </el-form-item>
           </el-form>
       </el-card>
@@ -25,7 +25,49 @@
 
 <script>
 export default {
-
+  data () {
+    let validator = function (rule, value, callBack) {
+      if (value) {
+        callBack()
+      } else {
+        callBack(new Error('请阅读并同意用户协议和隐私条款'))
+      }
+    }
+    return {
+      loginform: {
+        mobile: '',
+        number: '',
+        status: false
+      },
+      loginrules: {
+        mobile: [{ required: true, message: '电话号码不能为空' }, {
+          pattern: /^1[3456789]\d{9}$/, message: '电话号码为十一位数字'
+        }],
+        number: [{ required: true, message: '验证码不能为空' }, {
+          pattern: /^\d{6}$/, message: '验证码为六位数字'
+        }],
+        status: [{
+          validator
+        }]
+      }
+    }
+  },
+  methods: {
+    checkall () {
+      this.$refs.login.validate(isOk => {
+        if (isOk) {
+          alert('right')
+        } else {
+          this.$message({
+            type: 'warning',
+            message: '请正确填写信息'
+          })
+          this.loginform.mobile = ''
+          this.loginform.number = ''
+        }
+      })
+    }
+  }
 }
 </script>
 

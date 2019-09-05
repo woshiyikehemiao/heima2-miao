7b6c39370e98633b545e15643cefc3630fcafcaa
崔苗苗 <1940803799@qq.com>
2019-09-05 21:29:23 +0800

    评论列表，页面结构，table的使用

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,12 +1,44 @@
 <template>
-  <div>
-      评论管理
-  </div>
+  <el-card>
+      <bread-crumb slot="header">
+        <template slot="title">
+            内容列表
+        </template>
+      </bread-crumb>
+      <el-table
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="标题"
+        width="700">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="评论状态">
+      </el-table-column>
+      <el-table-column
+        prop="all"
+        label="总评论数">
+      </el-table-column>
+      <el-table-column
+        prop="fans"
+        label="粉丝评论数">
+      </el-table-column>
+      <el-table-column
+        label="操作">
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  }
 }
 </script>
 

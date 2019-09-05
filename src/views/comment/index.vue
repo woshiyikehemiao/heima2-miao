75555c944b8f8802260b7cfd900e091f63dd9b02
崔苗苗 <1940803799@qq.com>
2019-09-05 22:46:25 +0800

    列表分页及请求

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,40 +1,27 @@
 <template>
   <el-card>
-      <bread-crumb slot="header">
-        <template slot="title">
-            内容列表
-        </template>
-      </bread-crumb>
-      <el-table
-      :data="list"
-      style="width: 100%">
-      <el-table-column
-        prop="title"
-        label="标题"
-        width="660">
-      </el-table-column>
-      <el-table-column
-        :formatter="formatter"
-        prop="comment_status"
-        label="评论状态">
-      </el-table-column>
-      <el-table-column
-        prop="total_comment_count"
-        label="总评论数">
-      </el-table-column>
-      <el-table-column
-        prop="fans_comment_count"
-        label="粉丝评论数">
-      </el-table-column>
-      <el-table-column
-        label="操作"
-        width="160">
+    <bread-crumb slot="header">
+      <template slot="title">内容列表</template>
+    </bread-crumb>
+    <el-table :data="list" style="width: 100%">
+      <el-table-column prop="title" label="标题" width="660"></el-table-column>
+      <el-table-column :formatter="formatter" prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+      <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+      <el-table-column label="操作" width="160">
         <template slot-scope="obj">
-        <el-button type="text" size="mini">修改评论</el-button>
-        <el-button type="text" size="mini" @click="changestatus(obj.row)">{{obj.row.comment_status?'关闭':'打开'}}评论</el-button>
+          <el-button type="text" size="mini">修改评论</el-button>
+          <el-button
+            type="text"
+            size="mini"
+            @click="changestatus(obj.row)"
+          >{{obj.row.comment_status?'关闭':'打开'}}评论</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <el-row type="flex" justify="center">
+    <el-pagination :total="page.total" :current-page="page.page" :page-size="page.size" @current-change='changepage'></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -42,10 +29,19 @@
 export default {
   data () {
     return {
+      page: {
+        page: 1,
+        size: 10,
+        total: 0
+      },
       list: []
     }
   },
   methods: {
+    changepage (newpage) {
+      this.page.page = newpage
+      this.getList()
+    },
     changestatus (row) {
       let mess = row.comment_status ? '关闭' : '打开'
       this.$confirm(`你确定要${mess}评论吗`, '提示').then(res => {
@@ -70,10 +66,13 @@ export default {
       this.$axios({
         url: '/articles',
         params: {
-          response_type: 'comment'
+          response_type: 'comment',
+          per_page: this.page.size,
+          page: this.page.page
         }
       }).then(res => {
         console.log(res)
+        this.page.total = res.data.total_count
         this.list = res.data.results
       })
     }
@@ -85,5 +84,4 @@ export default {
 </script>
 
 <style>
-
 </style>

17454fe588aa14bc85c6d66cf49df314718a08b3
崔苗苗 <1940803799@qq.com>
2019-09-05 20:46:37 +0800

    左侧导航配置路由

--- src/components/layout-aside.vue
+++ src/components/layout-aside.vue
@@ -3,6 +3,7 @@
       class="menu"
       background-color=" #323745"
       text-color="#adafb5"
+      :router="true"
       active-text-color="#ffd04b">
       <div class="logo">
           <img src="../../src/assets/img/logo_admin.png" alt="">
@@ -16,20 +17,20 @@
               <i class="el-icon-s-unfold"></i>
               <span>内容管理</span>
               </template>
-          <el-menu-item index="1-1">发布文章</el-menu-item>
-          <el-menu-item index="1-2">内容列表</el-menu-item>
-          <el-menu-item index="1-3">评论管理</el-menu-item>
-          <el-menu-item index="1-4">素材管理</el-menu-item>
+          <el-menu-item index="/home/publish">发布文章</el-menu-item>
+          <el-menu-item index="/home/articles">内容列表</el-menu-item>
+          <el-menu-item index="/home/comment">评论管理</el-menu-item>
+          <el-menu-item index="/home/material">素材管理</el-menu-item>
         </el-submenu>
         <el-submenu index="2">
           <template slot="title">
               <i class="el-icon-s-custom"></i>
               <span>粉丝管理</span>
               </template>
-          <el-menu-item index="2-1">图文数据</el-menu-item>
-          <el-menu-item index="2-2">粉丝概况</el-menu-item>
-          <el-menu-item index="2-3">粉丝列表</el-menu-item>
-          <el-menu-item index="2-4">粉丝画像</el-menu-item>
+          <el-menu-item index="/home/fansdata">图文数据</el-menu-item>
+          <el-menu-item index="/home/fansinfo">粉丝概况</el-menu-item>
+          <el-menu-item index="/home/fanslist">粉丝列表</el-menu-item>
+          <el-menu-item index="/home/fansimg">粉丝画像</el-menu-item>
         </el-submenu>
       <el-menu-item index="/money">
         <i class="el-icon-setting"></i>

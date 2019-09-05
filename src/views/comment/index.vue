85fba066cc6c7b23aeed25b12fe3edf0366471d9
崔苗苗 <1940803799@qq.com>
2019-09-05 21:46:10 +0800

    评论列表，评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -9,24 +9,28 @@
       :data="list"
       style="width: 100%">
       <el-table-column
-        prop="date"
+        prop="title"
         label="标题"
-        width="700">
+        width="660">
       </el-table-column>
       <el-table-column
-        prop="name"
+        :formatter="formatter"
+        prop="comment_status"
         label="评论状态">
       </el-table-column>
       <el-table-column
-        prop="all"
+        prop="total_comment_count"
         label="总评论数">
       </el-table-column>
       <el-table-column
-        prop="fans"
+        prop="fans_comment_count"
         label="粉丝评论数">
       </el-table-column>
       <el-table-column
-        label="操作">
+        label="操作"
+        width="160">
+        <el-button type="text" size="mini">修改评论</el-button>
+        <el-button type="text" size="mini">o/c评论</el-button>
       </el-table-column>
     </el-table>
   </el-card>
@@ -38,6 +42,25 @@ export default {
     return {
       list: []
     }
+  },
+  methods: {
+    formatter (row) {
+      return row ? '正常' : '关闭'
+    },
+    getList () {
+      this.$axios({
+        url: '/articles',
+        params: {
+          response_type: 'comment'
+        }
+      }).then(res => {
+        console.log(res)
+        this.list = res.data.results
+      })
+    }
+  },
+  created () {
+    this.getList()
   }
 }
 </script>
